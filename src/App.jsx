6ae40867aaa5a24bde49d0934e5171b236aff167
Oleg Portnov <drose1@list.ru>
2025-04-27 21:30:31 +0400

    upd

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { Airplay } from 'lucide-react';
+
+const Div = styled.div`
+  text-align: center;
+`;
+
+function App() {
+  return (
+    <Div>
+      <Airplay />
+      123
+    </Div>
+  );
+}
+
+export default App;
